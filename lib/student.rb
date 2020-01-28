ad6dd0c59620dd592f6b4e10e70b9ef49c1d3981
Lydia B <lydiabaik@gmail.com>
2020-01-28 00:32:25 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+require_relative '../lib/user.rb'
 
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initialize(first_name = nil, last_name = nil)
+    @first_name = first_name
+    @last_name = last_name
+    @knowledge = []
+  end
+
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
