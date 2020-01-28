4b8d814d4dd523884f72b1540c86fcc830dec7d5
Lydia B <lydiabaik@gmail.com>
2020-01-28 00:28:56 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,10 @@
 class User
   attr_accessor :first_name, :last_name
+  @@knowledge = []
 
   def initialize(first_name = nil, last_name = nil)
     @first_name = first_name
     @last_name = last_name
-    @@knowledge = []
   end
 
 end
